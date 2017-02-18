1814dcfbc92b9fe55ebb707c26f33b12781b007c
Yegor Putikhin <puteg@yandex.ru>
2017-02-18 10:31:04 +0300

    Create
    Create Mvc Application
    Configure Azure

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DomOffline.Startup))]
+namespace DomOffline
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
